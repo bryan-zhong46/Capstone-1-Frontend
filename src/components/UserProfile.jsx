e851977078afbdd37ee440a24dc81e1b2bb6b220
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-07-18 11:08:26 -0400

    Added admin and disable account checkboxes

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./NavBarStyles.css";
+import "./Profile.css";
 import { API_URL } from "../shared";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink, useNavigate } from "react-router-dom";
 
 const UserProfile = ({ user }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
+  //const location = useLocation();
+  /*
+  const [isEditing, setIsEditing] = useState(
+    location.state?.isEditing || false
+  );*/
+
   const userID = Number(id); //convert id from string to number
 
   const [isUser, setIsUser] = useState(false);
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({
+    isAdmin: user.isAdmin,
+    isDisabled: user.isDisabled,
+  });
 
-  console.log(userID);
+  //For admin users, add the ability to disable an account, or make a account admin
 
   const checkUser = () => {
     if (user?.id == userID) {
@@ -22,6 +33,7 @@ const UserProfile = ({ user }) => {
     }
   };
 
+  /*
   const updateUser = async () => {
     if (isUser) {
       try {
@@ -34,26 +46,74 @@ const UserProfile = ({ user }) => {
         console.log(error);
       }
     }
-  };
+  }; */
+
+  // edit user
+  //const editUser = async () => {};
 
   useEffect(() => {
-    //handleView;
     checkUser();
   }, [user]);
 
-  useEffect(() => {
-    if (isUser) {
-      setUserData({
-        email: "TestUser@gmail.com",
-      });
+  const fetchUser = async () => {
+    const res = await axios.get(`${API_URL}/api/users/${id}`);
+    setUserData(res.data);
+  };
+
+  const handleAdminChange = async (e) => {
+    const adminState = e.target.checked;
+    setUserData((prev) => ({
+      ...prev,
+      isAdmin: adminState,
+    }));
+  };
+
+  const handleDisableChange = async (e) => {
+    const disableState = e.target.checked;
+    setUserData((prev) => ({
+      ...prev,
+      isDisabled: disableState,
+    }));
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.patch(
+        `${API_URL}/api/users/${id}`,
+        userData
+      );
+      fetchUser();
+      console.log("Status: ", response.status);
+    } catch (error) {
+      console.error(error);
     }
-  }, []);
+  };
 
   return (
     <div>
-      {isUser ? (
+      {isUser || user.isAdmin ? (
         <div>
-          <p>User</p>
+          {user.isAdmin ? (
+            <div>
+              <form>
+                <input
+                  type="checkbox"
+                  checked={userData.isAdmin}
+                  onChange={handleAdminChange}
+                />
+                <label>Make this user Admin?</label>
+                <input
+                  type="checkbox"
+                  checked={userData.isDisabled}
+                  onChange={handleDisableChange}
+                />
+                <label>Disable this account?</label>
+              </form>
+              <button onClick={handleSave}>Save</button>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <div>
@@ -61,9 +121,17 @@ const UserProfile = ({ user }) => {
         </div>
       )}
 
-      <div>
-        <h1>{user.username}'s Profile</h1>
-        <button onClick={updateUser}>Confirm</button>
+      <div className="user-profile-card">
+        <div className="user-profile-title">
+          <h1>{user.username}'s Profile</h1>
+        </div>
+
+        <div className="user-profile-body">
+          <p>Username: {user.username}</p>
+          <p>UserID: {user.id}</p>
+          <p>Email: {user.email}</p>
+        </div>
+        <NavLink to="./">View Polls</NavLink>
       </div>
     </div>
   );
