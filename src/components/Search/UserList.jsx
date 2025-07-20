6ab338a0de02b887bc3bbce6fc494939bfc88fb0
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-07-19 21:24:44 -0400

    Styling for search list and navbar

--- src/components/Search/UserList.jsx
+++ src/components/Search/UserList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Search.css";
+import { Link } from "react-router-dom";
 
 const UserList = ({ users, text }) => {
   const filteredUsers = users.filter((user) => {
@@ -14,8 +15,12 @@ const UserList = ({ users, text }) => {
     <div className="users-list">
       <div className="users-list-card">
         {filteredUsers.map((listUser) => (
-          <div key={listUser.id} className="user-list-item">
-            <span>{listUser.username}</span>
+          <div key={listUser.user_id} className="user-list-item">
+            <span>
+              <Link to={`/users/${listUser.user_id}`} className="link-list">
+                {listUser.username}
+              </Link>
+            </span>
           </div>
         ))}
       </div>
