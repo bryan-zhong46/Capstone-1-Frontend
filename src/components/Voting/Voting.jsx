99db73a459f6ac5d6354857b12f3c42b536e7814
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-07-22 14:29:56 -0400

    fetching polls

--- src/components/Voting/Voting.jsx
+++ src/components/Voting/Voting.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../../shared";
+import axios from "axios";
 
 const Voting = () => {
-  return <></>;
+  const { id } = useParams();
+  const pollID = Number(id);
+  const [poll, setPoll] = useState(null);
+
+  useEffect(() => {
+    const fetchPoll = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/polls/${id}`);
+        setPoll(response);
+        console.log(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchPoll();
+  }, [pollID, poll]);
+
+  return <div></div>;
 };
 
 export default Voting;
